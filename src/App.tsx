eb9cfae62f64d2371e818f1d868871733488964c
TranVanNghia <TranNghia170735@gmail.com>
2022-05-26 22:08:44 +0700

    Init learn-ts repo

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Footer, Header } from './components/common';
+function App() {
+  return (
+    <div>
+        <Header></Header>
+        <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
